9f6bd4412381bfc83d7dfabad54e6d303f72e12a
AdiyieR26 <radoine1996@gmail.com>
2024-08-20 12:53:27 +0200

    solved exercise

---
+++ firstLine/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello World!");
+// console.WriteLine("Hello World!");
+// Console.WriteLine('Hello World!');
+// Console.WriteLine("Hello World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+
